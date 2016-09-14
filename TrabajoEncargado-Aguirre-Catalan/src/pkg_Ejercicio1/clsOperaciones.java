4eb3e29770c7dafb62ba9c726bd15f857fb7106e
Drei109 <drei.rar@gmail.com>
2016-09-14 17:43:38 -0500

    Code cleared

--- TrabajoEncargado-Aguirre-Catalan/src/pkg_Ejercicio1/clsOperaciones.java
+++ TrabajoEncargado-Aguirre-Catalan/src/pkg_Ejercicio1/clsOperaciones.java
@@ -10,14 +10,10 @@ public class clsOperaciones {
     //VARIABLES
     double a, b, resultado;
     String operacion = "";
-    int id; //CONTADOR PARA EL ID DE CADA OPERACION GUARDADA EN EL FICHERO
+    
     
     //ARCHIVO
     File ficheroOperaciones = new File("operaciones.txt");
-    
-    //CREACION DE ARRAY LIST DE DATOS (OBJETO)
-    ArrayList<clsOperaciones> datosOperaciones = new ArrayList<clsOperaciones>();
-    clsOperaciones objeto = null;
     
     //CONSTRUCTOR SIN PARAMETROS
     public clsOperaciones(){}
@@ -63,9 +59,6 @@ public class clsOperaciones {
             //MESNAJE DE CONFIRMACION DE QUE LA CADENA SE GUARDO
             JOptionPane.showMessageDialog(null, "Datos Guardados Correctamente");
             
-            //ID AUMENTA EN 1
-//            id++;
-            
             //CERRAMOS EL FLUJO
             escribirArchivo.close();
             
@@ -73,59 +66,6 @@ public class clsOperaciones {
             JOptionPane.showMessageDialog(null, "¡Error al Insertar los datos!");
         }
     }
-    
-    //AGREGAR LOS DATOS DEL FICHERO AL OBJEETO (ARRAY LIST)
-    public void ficheroAObjeto(){
-        try {
-            //VARIBLE QUE TENDRA LINEA DE FICHERO
-            String linea = null;
-            
-            //FLUJO DE LECTURA DE FICHERO
-            BufferedReader leerFichero = new BufferedReader
-                                            (new FileReader(ficheroOperaciones));
-            
-            //OBTENDREMOS TODAS LAS LINEAS DEL FICHERO
-            while ((linea = leerFichero.readLine()) != null) {
-                //SEPARAREMOS LOS DATOS POR CADA TABULACION ENCONTRADA EN LA VARIABLE LINEA
-                StringTokenizer separadoPor = new StringTokenizer(linea,"\t");
-                String id        = separadoPor.nextToken().trim();
-                String a         = separadoPor.nextToken().trim();
-                String b         = separadoPor.nextToken().trim();
-                String operacion = separadoPor.nextToken().trim();
-                String resultado = separadoPor.nextToken().trim();
-                
-                //CONVERSION DE LOS DATOS QUE SE NECESITAN
-                int idOperacion = Integer.parseInt(id);
-                double valorA = Double.parseDouble(a);
-                double valorB = Double.parseDouble(b);
-                double valorResultado = Double.parseDouble(resultado);
-                
-                //ASIGNACION DE LOS DATOS DEL FICHERO, A LA CLASE MEDIANTE EL CONSTRUCTOR POR PARAMETROS
-                objeto = new clsOperaciones(idOperacion, valorA, valorB, operacion, valorResultado);
-                
-                //AGREGAMOS LOS DATOS SACADOS AL ARRAY LIST QUE USAREMOS PARA LA MANIPULACION DE LOS DATOS
-                datosOperaciones.add(objeto);
-            }
-            
-            //CIERRE DEL FLUJO DE LECTURA DEL FICHERO
-            leerFichero.close();
-            
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, "¡Error al agregar los datos del fichero al array list!");
-        }
-    }
-    
-    //ACTUALIZAR
-    public void actualizarArray(){
-        //LIMPIAR EL ARRAY LIST
-        datosOperaciones.clear();
-        
-        //VOLVER A SACAR LOS DATOS DEL FICHERO
-        ficheroAObjeto();
-    }
-    
-    //MOSTRAR DATOS DEL FICHERO
-    
     
     //CALCULAR EL RESULTADO
     public double obtenerResultado(String operacion,double a, double b){
